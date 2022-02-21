1a70240140ae7be90176e895ea36fc8d45a702e3
Usuario <a@gmail.com>
2022-02-21 15:39:10 -0600

    Add CustomCrate

---
+++ src/hcf/crates/types/Crate.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace hcf\crates\types;
+
+use hcf\crates\utils\CratesUtils;
+use hcf\Loader;
+use pocketmine\utils\Config;
+
+class Crate
+{
+
+    public ?string $crateName = "null";
+    public array $items = [];
+    public Config $config;
+
+    public function __construct(string $crateName, array $items)
+    {
+        $this->setItems($items);
+        $this->setCrateName($crateName);
+        $this->setConfig(new Config(Loader::getInstance()->getDataFolder()."crates".DIRECTORY_SEPARATOR.$crateName.".yml", Config::YAML));
+        $configItems = [];
+        foreach($this->getItems() as $slot => $item){
+            $configItems[$slot] = CratesUtils::itemSerialize($item);
+        }
+        $this->getConfig()->set("name", $this->getCrateName());
+        $this->getConfig()->set("items", $configItems);
+        $this->getConfig()->save();
+    }
+
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setConfig(Config $config): void
+    {
+        $this->config = $config;
+    }
+
+    public function getConfig(): Config
+    {
+        return $this->config;
+    }
+
+    public function setCrateName(?string $crateName): void
+    {
+        $this->crateName = $crateName;
+    }
+
+    public function getCrateName(): ?string
+    {
+        return $this->crateName;
+    }
+
+}
--- src/hcf/crates/utils/CratesUtils.php
+++ src/hcf/crates/utils/CratesUtils.php
@@ -2,7 +2,37 @@
 
 namespace hcf\crates\utils;
 
+use hcf\PlayerHCF;
+use libs\invmenu\InvMenu;
+use libs\invmenu\transaction\InvMenuTransaction;
+use libs\invmenu\type\InvMenuTypeIds;
+use pocketmine\item\Item;
+use pocketmine\utils\TextFormat;
+
 class CratesUtils
 {
+
+    public static function openLootCrateEdit(string $crate, PlayerHCF $playerHCF)
+    {
+        $menu = InvMenu::create(InvMenuTypeIds::TYPE_CHEST);
+        $menu->setName(TextFormat::colorize("&cLoot Crate Edit"));
+        $menu->setListener(function (InvMenuTransaction $transaction){
+            $transaction->continue();
+        });
+        $menu->setInventoryCloseListener(function() use ($crate, $menu, $playerHCF){
+            $contents = $menu->getInventory()->getContents();
+            $playerHCF->getInventory()->setContents($contents);
+        });
+    }
+
+    public static function itemSerialize(Item $item): array
+    {
+        return $item->jsonSerialize();
+    }
+
+    public static function itemDeserialize(array $items): Item
+    {
+        return Item::jsonDeserialize($items);
+    }
 
 }
