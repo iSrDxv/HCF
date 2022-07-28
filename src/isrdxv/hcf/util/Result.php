2378f1304e6b1b2fb8745d1e8e70879ead58736a
srclauyt@gmail.com <srclauyt@gmail.com>
2022-07-27 21:52:17 -0300

    update Result.php

---
+++ src/isrdxv/hcf/util/Result.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace isrdxv\hcf\util;
+
+use pocketmine\item\Item;
+use pocketmine\tag\nbt\CompoundTag;
+
+class Result
+{
+  
+  public function encodeItem(Item $item): array
+  {
+    return $item->jsonSerialize();
+  }
+  
+  public function encodeItems(array $contents): array
+  {
+    if (empty($contents)) {
+      return;
+    }
+    $data = [];
+    foreach($contents as $slot => $item) {
+      $data[$slot] = $item->jsonSerialize();
+    }
+    return $data;
+  }
+  
+  public function decodeItem(array $data): Item
+  {
+    return Item::jsonDeserealize($data);
+  }
+  
+  public function decodeItems(array $contents): array
+  {
+    $items = [];
+    foreach($contents as $slot => $data) {
+      $items[$slot] = Item::jsonDeserealize($data);
+    }
+    return $items;
+  }
+  
+}
