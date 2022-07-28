8051b4d0e3e52bbb2092386cf035a2e1e68cb7f3
srclauyt@gmail.com <srclauyt@gmail.com>
2022-07-28 09:20:41 -0300

    added Tower

---
+++ src/isrdxv/hcf/utils/BlocksHCF.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace isrdxv\hcf\utils;
+
+use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
+use pocketmine\network\mcpe\protocol\types\BlockPosition;
+use pocketmine\network\mcpe\convert\RuntimeBlockMapping;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\world\Position;
+
+class BlocksHCF
+{
+  
+  public function createTower(Player $player, Position $position): void
+  {
+    for($y = $position->getY(); $y < $position->getY() + 10; $y++) {
+      if (!$position->getWorld()->getBlock(($position = $position->add(0, $y, 0))) instanceof Air) {
+        continue;
+      }
+      $pk = UpdateBlockPacket::create(BlockPosition::fromVector3($position), RuntimeBlockMapping::getInstance()->toRuntimeId(VanillaBlocks::GLASS()->getFullId()), UpdateBlockPacket::FLAG_NETWORK, UpdateBlockPacket::DATA_LAYER_NORMAL);
+      $player->getNetworkSession()->sendDataPacket($pk);
+    }
+  }
+  
+  public function createWall(): void;
+  
+}
