da31546ef910d225c470054e28b6b464ee69e3d2
srclauyt@gmail.com <srclauyt@gmail.com>
2022-08-03 12:46:32 -0300

    prepare commands

--- src/isrdxv/hcf/crate/block/CrateChest.php
+++ src/isrdxv/hcf/crate/block/CrateChest.php
@@ -15,6 +15,7 @@ use pocketmine\block\{
 };
 use pocketmine\block\tile\Chest as TileChest;
 use pocketmine\player\Player;
+use pocketmine\item\ItemFactory;
 
 class CrateChest
 {
@@ -23,7 +24,7 @@ class CrateChest
   private Crate $crate;
   
   /** @var Item[] **/
-  private array $items;
+  private array $items = [];
   
   public function __construct(string $blockId, Crate $crate)
   {
@@ -32,14 +33,32 @@ class CrateChest
     parent::__construct(new BlockIdentifier(BlockLegacyIds::CHEST, 0, null, TileChest::class), "CrateChest", new BlockBreakInfo(2.5, BlockToolType::AXE));
   }
   
+  public function getBlockId(): array
+  {
+    return explode(":", $this->blockId);
+  }
+  
   public function toShow(Player $player): void
   {
     $menu = InvMenu::create(InvMenu::TYPE_CHEST);
+    $menu->setName($this->crate->getName());
+    for($i = count($this->items) - 1; $i <= 52; $i++) {
+      if (empty($this->items[$i])) {
+        $this->items[$i] = ItemFactory::getInstance()->air();
+      }
+    }
+    $menu->setContents($this->items);
+    $menu->setListener(InvMenu::readonly());
+    $menu->send($player);
   }
   
   public function toUse(Player $player): void
   {
-    
+    if (count($this->items) === 0) {
+      return;
+    }
+    $item = $this->items[array_rand($this->items)];
+    $player->getInventory()->addItem($item);
   }
   
 }
